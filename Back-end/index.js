00a06a295ed7fdd369561ca1ef14a4570839bc55
Laise France <laise94@hotmail.com>
2023-01-04 16:51:17 -0300

    conectando a pasta public html ao back com o PATH

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+// path para integrar com o front-end
+const path = require('path');
 const app = express();
 
 const port = 3000;
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ROTA
 app.get('/', (req, res) => {
