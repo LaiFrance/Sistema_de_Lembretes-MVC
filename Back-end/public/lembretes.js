0dd5e37524ce7b3b8547354a4fe6f7c85bddbc45
Laise France <laise94@hotmail.com>
2023-01-04 19:49:41 -0300

    criando funções do javascript

--- Back-end/public/lembretes.js
+++ Back-end/public/lembretes.js
@@ -1,1 +1,43 @@
-alert("ola mundo")
+document.addEventListener('DOMContentLoaded', function() {
+        console.log('DOM carregado com sucesso!')
+       
+        // botão adicionar lembrete
+        document.getElementById('btnAdd').addEventListener('click', function() {
+            addLembrete();
+        });
+  });
+
+  // função para verificar se há texto no input
+    function checkInput(texto) {
+        if (texto == null || texto == "" || texto.lenght < 1 ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+ // mostrar erro 
+    function showError() {  
+        let html = "";
+        html += '<div class="alert alert-danger" role="alert">';
+        html += 'Por favor, digite um lembrete!';
+        html += '</div>';
+
+        document.getElementById('error').innerHTML = html;
+        
+    }
+
+    // função limpar erro
+    function clearError() {
+        document.getElementById('error').innerHTML = "";
+    }
+
+    // função para adicionar lembrete
+    function addLembrete() {
+       let texto = document.getElementById('texto').value;
+         if (!checkInput(texto)) {
+            showError();
+        } else {
+            clearError();
+        }
+            
+    }
