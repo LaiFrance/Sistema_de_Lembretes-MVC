0bb27f0ba5947cca2fe198eba7ce38f2f49f2d41
Laise France <laise94@hotmail.com>
2023-01-04 16:17:32 -0300

    criando pastas do backend e chamando index

--- Back-end/index.js
+++ Back-end/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+const port = 3000;
+app.set('view engine', 'ejs');
+
 // ROTA
 app.get('/home', (req, res) => {
-  res.send('Hello World');
+  res.render("index");
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    });
