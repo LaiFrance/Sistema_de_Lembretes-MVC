f3e92f630229cda55df27343b414ccb8e6bf03a9
Laise France <laise94@hotmail.com>
2023-01-05 11:26:53 -0300

    configurando e conectando ao banco de dados MONGODB

---
+++ Back-end/database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const connectToDb = () => {
+  // string de conexão
+  mongoose
+    .connect(
+      'mongodb+srv://root:admin@lembretes.itnw3bc.mongodb.net/?retryWrites=true&w=majority',
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    )
+    .then(() => {
+      console.log('Conectado ao banco de dados! MONGODB ATLAS CONECTADO!')
+    })
+    .catch((err) => {
+      console.log('Erro ao conectar ao banco de dados: ', err)
+    })
+}
+
+module.exports = connectToDb
--- Back-end/index.js
+++ Back-end/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 // path para integrar com o front-end
 const path = require('path');
 const routes = require('./routes/routes');
-const app = express();
+// conectar com o banco de dados
+const connectToDb = require('./database/db');
 
+connectToDb();
+const app = express();
 const port = 3000;
+
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes)
